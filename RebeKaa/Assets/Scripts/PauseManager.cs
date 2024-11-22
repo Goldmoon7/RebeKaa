7cf7360d6a78fe6f1ffa62ebc7c8e180a71823a0
Vadezzzz <victor.martinagu@alumnos.upm.es>
2024-11-22 16:57:04 +0100

    Menu primera parte

--- RebeKaa/Assets/Scripts/PauseManager.cs
+++ RebeKaa/Assets/Scripts/PauseManager.cs
@@ -6,40 +6,37 @@ using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
-    public GameObject PauseMenu;  // Referencia al menú de pausa en la UI
-    public GameObject SettingsMenu; //Referencia al menu de ajustes
-    //private bool isPaused = false;  // Indica si el juego está en pausa
-    public bool PauseActivo = false;
-    public bool SettingsActivo = false;
-
-    void Start() {
-        PauseMenu.SetActive(false);
-        SettingsMenu.SetActive(false);
-    }
+    public GameObject Menu;  // Referencia al menú de pausa en la UI
+    public GameObject AjustesIN; //Referencia al menu de ajustes
+    private bool isPaused = false;  // Indica si el juego está en pausa
+    private bool MenuActivo = false;
+    private bool AjustesActivo = false;
 
     void Update()
     {
         // Detectar si se presiona la tecla Escape
       if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if(PauseActivo && !SettingsActivo){
-                ResumeGame();
-            } else if(!SettingsActivo && !PauseActivo){
-                PauseGame();
-            } else if(SettingsActivo && PauseActivo){
+            if(MenuActivo && !AjustesActivo){
                 ResumeGame();
             }
             /*if(AjustesActivo && !MenuActivo){
                 SalirDeAjustes();
             }*/
+            if(!AjustesActivo && !MenuActivo){
+                PauseGame();
+            }
+            if(AjustesActivo && MenuActivo){
+                ResumeGame();
+            }
         }
     }
 
     // Método para pausar el juego
     public void PauseGame()
     {
-        PauseMenu.SetActive(true);  // Mostrar el menú de pausa
-        PauseActivo = true;
+        Menu.SetActive(true);  // Mostrar el menú de pausa
+        MenuActivo = true;
         Time.timeScale = 0f;   // Detener el tiempo en el juego
         Debug.Log("Juego pausado"); // Mensaje de depuración
     }
@@ -47,10 +44,10 @@ public class PauseManager : MonoBehaviour
     // Método para reanudar el juego
     public void ResumeGame()
     {
-        PauseMenu.SetActive(false); // Ocultar el menú de pausa
-        PauseActivo = false;
-        SettingsMenu.SetActive(false);
-        SettingsActivo = false;
+        Menu.SetActive(false); // Ocultar el menú de pausa
+        MenuActivo = false;
+        AjustesIN.SetActive(false);
+        AjustesActivo = false;
         Time.timeScale = 1f;   // Reanudar el tiempo en el juego
         Debug.Log("Juego reanudado"); // Mensaje de depuración
     }
@@ -63,14 +60,14 @@ public class PauseManager : MonoBehaviour
     }
 
     public void IrAjustes(){
-        SettingsMenu.SetActive(true);
-        SettingsActivo = true;
+        AjustesIN.SetActive(true);
+        AjustesActivo = true;
         Time.timeScale = 0f;   // Detener el tiempo en el juego
     }
 
     public void SalirDeAjustes(){
-        SettingsMenu.SetActive(false);
-        SettingsActivo = false;
+        AjustesIN.SetActive(false);
+        AjustesActivo = false;
         Time.timeScale = 0f;   // Detener el tiempo en el juego
     }
 
