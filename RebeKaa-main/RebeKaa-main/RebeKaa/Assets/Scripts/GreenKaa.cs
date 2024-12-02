addf83045aa0cc8060753020433fc5993b78e22f
Vadezzzz <victor.martinagu@alumnos.upm.es>
2024-12-02 11:52:45 +0100

    Nuevos Ficheros

---
+++ RebeKaa-main/RebeKaa-main/RebeKaa/Assets/Scripts/GreenKaa.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenKaa : MonoBehaviour
+{
+
+    public GameObject greenKaaPrefab;
+    private int xlimit = 35;
+    private int ylimit = 16;
+
+    static public bool fly = false; //Indica si Kaa esta volando o no
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnGreenKaa",0f,10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnGreenKaa() {
+        int x = Random.Range(-xlimit,xlimit);
+        int y = Random.Range(-ylimit,ylimit);
+        Vector3 pos = new Vector3(x,y,0);
+        GameObject newSegment = Instantiate(greenKaaPrefab, pos, Quaternion.identity);
+    }
+}
+
