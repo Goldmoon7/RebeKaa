82c0e5925ecbc2a220b3f2dcd2f89c56ebe5c2e2
Vadezzzz <victor.martinagu@alumnos.upm.es>
2024-12-31 19:22:29 +0100

    version2 vidas infinitas

--- RebeKaa/Assets/Scripts/ModoInfinito.cs
+++ RebeKaa/Assets/Scripts/ModoInfinito.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA;
 
 public class ModoInfinito : MonoBehaviour
 {
@@ -22,6 +24,16 @@ public class ModoInfinito : MonoBehaviour
     }
     public void Modo_Infinito(){
         noMorir = !noMorir;
+        if(noMorir == false){
+            GameObject go = GameObject.FindGameObjectWithTag("Activacion");
+            go.GetComponent<Text>().text = "OFF";
+            go.GetComponent<Text>().color = Color.red;
+        }
+        else{
+            GameObject go = GameObject.FindGameObjectWithTag("Activacion");
+            go.GetComponent<Text>().text = "ON";
+            go.GetComponent<Text>().color = new Color(0.0667f, 0.3961f, 0.0157f, 1.0f);
+        }
         foreach(GameObject corazon in Snake.hearts){
             if(corazon != null){
                 SpriteRenderer spriteRenderer = corazon.GetComponent<SpriteRenderer>();
