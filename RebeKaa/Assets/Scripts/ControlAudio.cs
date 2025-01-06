02aa6af57c5628f7b54b0293a6198494d4e5fc4b
Vadezzzz <victor.martinagu@alumnos.upm.es>
2025-01-06 23:38:26 +0100

    sliders de sonido implementadas

--- RebeKaa/Assets/Scripts/ControlAudio.cs
+++ RebeKaa/Assets/Scripts/ControlAudio.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlAudio : MonoBehaviour
 {
     public static ControlAudio Instance;
-    private AudioSource audioSource;
 
+    // Asignar los diferentes AudioSource en el Inspector
+    public AudioSource sonidoAmbiente;
+    public AudioSource sonidoFruta, sonidoGreenKaa, sonidoEnemigo, sonidoDaño;
+
+    // Sliders para controlar el volumen
+    public Slider volumenFrutasGreenKaa; // Slider para Fruta y GreenKaa
+    public Slider volumenEnemigosDaño;   // Slider para Enemigo y Daño
     private void Awake(){
         if(Instance == null){
             Instance = this;
@@ -15,10 +22,47 @@ public class ControlAudio : MonoBehaviour
         else{
             Destroy(gameObject);
         }
-        audioSource=GetComponent<AudioSource>();
+        volumenFrutasGreenKaa.value = 0.359f;
+        volumenEnemigosDaño.value = 0.7f;
+
+        volumenFrutasGreenKaa.onValueChanged.AddListener(AjustarVolumenFrutasGreenKaa);
+        volumenEnemigosDaño.onValueChanged.AddListener(AjustarVolumenEnemigosDaño);
     }
 
+        // Ajustar el volumen de los sonidos de frutas y GreenKaa
+    public void AjustarVolumenFrutasGreenKaa(float volumen){
+        sonidoFruta.volume = volumen;
+        sonidoGreenKaa.volume = volumen;
+    }
+
+    // Ajustar el volumen de los sonidos de enemigos y daño
+    public void AjustarVolumenEnemigosDaño(float volumen){
+        sonidoEnemigo.volume = volumen;
+        sonidoDaño.volume = volumen;
+    }
+
+    // Ejecutar sonido de ambiente
     public void EjecutarSonido(AudioClip sonido){
-        audioSource.PlayOneShot(sonido);
+        sonidoAmbiente.PlayOneShot(sonido);
+    }
+
+    // Ejecutar sonido de la fruta
+    public void EjecutarSonidoFruta(AudioClip sonido){
+        sonidoFruta.PlayOneShot(sonido);
+    }
+
+    // Ejecutar sonido de GreenKaa
+    public void EjecutarSonidoGreenKaa(AudioClip sonido){
+        sonidoGreenKaa.PlayOneShot(sonido);
+    }
+
+    // Ejecutar sonido de enemigo
+    public void EjecutarSonidoEnemigo(AudioClip sonido){
+        sonidoEnemigo.PlayOneShot(sonido);
+    }
+
+    // Ejecutar sonido de daño
+    public void EjecutarSonidoDaño(AudioClip sonido){
+        sonidoDaño.PlayOneShot(sonido);
     }
 }
