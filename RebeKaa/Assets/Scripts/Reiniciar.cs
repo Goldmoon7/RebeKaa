005eef5112925981ed2f21f3e287344be0bc1397
rodrigo <rogamer2468+github@gmail.com>
2025-01-12 15:23:11 +0100

    mejora tutorial

---
+++ RebeKaa/Assets/Scripts/Reiniciar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reiniciar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("nivelActual",0);
+        SceneManager.LoadScene("MenuInicio");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
